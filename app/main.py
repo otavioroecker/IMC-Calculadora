46f832f5beac0ab8185f459014fcfce293137f2d
otavioroecker <142278451+otavioroecker@users.noreply.github.com>
2026-09-23 20:25:25 -0300

    Create main.py

---
+++ app/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+from app.routes import router
+
+app = FastAPI(title="ImcCalculadora API")
+
+app.include_router(router)
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
